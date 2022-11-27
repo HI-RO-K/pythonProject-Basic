a80cb85bbb5d35f98c39033a5d830d21430dd1ba
HI-RO-K <I.hiroki.0509@outlook.jp>
2022-11-27 21:48:51 +0900

    Initial commit

---
+++ string_basic.py
@@ -0,0 +1,36 @@
+#文字列(String)について学習
+
+print("Hello world!!")
+#ダブルクォーテーションで囲むと文字列となる
+print('Hello world!!')
+#シングルクォーテーションでも文字列となる
+
+#データの型について
+print(1)
+print("1")
+#上は数字の1として扱い、下は文字の1として扱う
+
+print("I'm fine")
+print('I"m fine')
+
+#3つのダブルクォーテーションで囲むと複数の文字列を出力する
+print("""
+Hello world!
+How are you doing?""")
+
+#バックスラッシュnで改行
+print("Hello \nworld")
+
+#バックスラッシュtでタブ
+print("Hello \tworld")
+
+#バックスラッシュを説明したいときはエスケープを使う
+print("back slash n: \\n")
+print('I\'m fine')
+
+#プラスで文字列を連結する
+print("hello" + "world" + "!!")
+
+
+
+
